67fc3c97129568f5fee5c4765631de2927225308
NagyeongHa <hnk3176@naver.com>
2022-06-02 20:20:00 +0900

    git login onSubmit 수정

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -12,9 +12,11 @@ function Login() {
 
   return (
     <div>
-      <input type='text' name='email' placeholder='아이디' />
-      <input type='text' name='password' placeholder='비밀번호' />
-      <button onSubmit={handleLogin}>로그인</button>
+      <form onSubmit={handleLogin}>
+        <input type='text' name='email' placeholder='아이디' />
+        <input type='text' name='password' placeholder='비밀번호' />
+        <button type='submit'>로그인</button>
+      </form>
       <Link to='/signup' variant='body2'>
         계정이 없습니까? 여기서 가입 하세요.
       </Link>
