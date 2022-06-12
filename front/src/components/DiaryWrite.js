7bdf726c85bc8d4efbe2925859ce519dbcbad56e
NagyeongHa <hnk3176@naver.com>
2022-06-12 23:14:08 +0900

    feat : 글작성 페이지 css

--- front/src/components/DiaryWrite.js
+++ front/src/components/DiaryWrite.js
@@ -1,8 +1,9 @@
 import { useCallback, useState } from "react";
 import { useRecoilValue } from "recoil";
+import styled from "styled-components";
 import { date } from "../recoil/diary";
 import { call } from "../service/ApiService";
-import "../styles/DiaryWrite.css";
+//import "../styles/DiaryWrite.css";
 
 function DiaryEdit() {
   const yyyymmdd = useRecoilValue(date);
@@ -40,20 +41,20 @@ function DiaryEdit() {
 
   return (
     <>
-      <div className='container'>
+      <Container>
         <div>
-          <input
+          <Input
             className='titlebox'
             type='text'
             name='title'
             onChange={onChangeDiryInfo}
-            placeholder='제목'
+            placeholder='제목을 입력해주세요.'
           />
         </div>
         <div>
-          <textarea
+          <Textarea
             className='contents'
-            placeholder='내용'
+            placeholder='내용을 입력해주세요'
             type='text'
             name='contents'
             cols='30'
@@ -62,12 +63,52 @@ function DiaryEdit() {
           />
         </div>
 
-        <button onClick={onButtonClick} className='btn'>
+        <Button onClick={onButtonClick} className='btn'>
           작성하기
-        </button>
-      </div>
+        </Button>
+      </Container>
     </>
   );
 }
 
+const Input = styled.input`
+  border-radius: 0.3rem;
+  border: 1px solid gray;
+  height: 1.6rem;
+  width: 90%;
+  padding: 0.4rem;
+  margin-bottom: 1.5rem;
+  font-weight: 600;
+`;
+
+const Textarea = styled.textarea`
+  border-radius: 0.3rem;
+  border: 1px solid gray;
+  width: 90%;
+  padding: 0.4rem;
+  overflow: scroll;
+  font-size: 0.9rem;
+  margin-bottom: 1.5rem;
+  line-height: 1.4rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  height: 93vh;
+  width: 100vw;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  border-radius: 0.3rem;
+  border: none;
+  width: 75%;
+  padding: 10px;
+  margin: 0 auto;
+  touch-action: auto;
+  background-color: rgb(253, 245, 232);
+`;
 export default DiaryEdit;
