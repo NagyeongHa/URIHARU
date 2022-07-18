1224967116fcb80f566c53993cd0d4c3065e08bf
NagyeongHa <hnk3176@naver.com>
2022-07-18 18:22:49 +0900

    chore: backendHost 주소 수정

--- front/src/config.js
+++ front/src/config.js
@@ -1,29 +1,11 @@
-// let backendHost;
-
-// const hostname = window && window.location && window.location.hostname;
-
-// if (hostname === "localhost") {
-//   backendHost = "http://localhost:8080";
-// }
-// else {
-//   backendHost = "https://uri-haru.herokuapp.com";
-// }
-
-// backendHost = "https://uri-haru.herokuapp.com";
-
-// export const API_BASE_URL = `${backendHost}`;
-
-
 let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://localhost:8080";
+  backendHost = "http://localhost:8080/uriharu";
 } else {
   backendHost = "https://uri-haru.herokuapp.com";
 }
-
-// backendHost = "https://uri-haru.herokuapp.com";
 
 export const API_BASE_URL = `${backendHost}`;
