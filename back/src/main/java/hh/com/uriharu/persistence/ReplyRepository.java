1b73aeb605d2af30e090cf78aa0af6e33d9ffb7b
heocat <bban777@gmail.com>
2022-07-21 19:18:46 +0900

    feat: 일기 삭제시 댓글도 삭제

--- back/src/main/java/hh/com/uriharu/persistence/ReplyRepository.java
+++ back/src/main/java/hh/com/uriharu/persistence/ReplyRepository.java
@@ -2,7 +2,10 @@ package hh.com.uriharu.persistence;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,9 @@ import hh.com.uriharu.model.ReplyEntity;
 public interface ReplyRepository extends JpaRepository<ReplyEntity,Long> {
     @Query("select r from ReplyEntity r where r.diary.dno = ?1")
     List<ReplyEntity> findReplyListByDno(Long dno);
+
+    @Transactional
+    @Modifying
+    @Query("delete from ReplyEntity r where r.diary.dno =?1")
+    void deleteByDno(Long dno);
 }
